45c272ff5b9d76a3208e3240892bc39dd55a99c2
Irvin <irvinvictoria@gmail.com>
2024-09-16 16:00:30 -0400

    Created a new build and fixed routing error on frontend

--- ReactServer/reactServer.js
+++ ReactServer/reactServer.js
@@ -4,7 +4,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+
+
 app.use(express.static(path.join(__dirname,'/build')));
+
+app.use((req, res, next) => {
+    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+        next();
+    } else {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        res.sendFile(path.join(__dirname, '/build', '/index.html'));
+    }
+});
 
 const port = 80;
 app.listen(port, () => {
